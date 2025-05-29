bb7aeb3c394219ac7d70631693a02527266ca77f
Kai <kaizheng9000@gmail.com>
2025-05-28 20:15:49 -0600

    Initial Commit

---
+++ MemorizeTests/MemorizeTests.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeTests.swift
+//  MemorizeTests
+//
+//  Created by Kai Zheng on 5/28/25.
+//
+
+import Testing
+@testable import Memorize
+
+struct MemorizeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
