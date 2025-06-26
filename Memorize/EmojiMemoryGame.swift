ab570a860d37f2c9c8d7592970ef7a1f6ff780ee
Kai <kaizheng9000@gmail.com>
2025-06-26 10:37:02 -0600

    Working on the model

---
+++ Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,8 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Kai Zheng on 6/26/25.
+//
+
+import Foundation
---
+++ Memorize/MemorizeGame.swift
@@ -0,0 +1,8 @@
+//
+//  MemorizeGame.swift
+//  Memorize
+//
+//  Created by Kai Zheng on 6/26/25.
+//
+
+import Foundation
